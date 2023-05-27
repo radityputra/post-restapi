71a09565011c7407f319531a26ed3763496530d5
Radit <radityaa.pr@gmail.com>
2023-05-27 15:42:27 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
-
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+require('dotenv/config')
+
+app.use(bodyParser.urlencoded)({
+  extended: true
+})
+app.use(bodyParser.json());
+app.use(cors())
 
 app.get('/', (req, res) => {
-  res.send('Halo Radit Gantenk XD!')
+  res.send('Hello World 3!')
 })
 
+mongoose.connect(process.env.DB_CONNECTION,{
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+let db = mongoose.connection
+
+//handle error
+db.on('error', console.error.bind(console,'Error estabilishing a databse connection?'))
+
+// handle success
+db.once('open', () => {
+  console.log('Database is connected')
+})
 app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Server running on port ${process.env.PORT}`)
 })
