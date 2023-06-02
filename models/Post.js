2b17a61404e3b2ad330e4398befa0cff7938b78d
Radit <radityaa.pr@gmail.com>
2023-06-02 18:38:49 +0700

    Update Post.js

--- models/Post.js
+++ models/Post.js
@@ -6,7 +6,8 @@ const postSchema = mongoose.Schema({
         required: true
     },
     user_id:{
-        type: String,
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
         required:true
     },
     created_date:{
